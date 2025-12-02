0668ead08cda7964837823f276ebcdffc077e4fc
Paige <paige.jh27@gmail.com>
2025-12-02 23:02:14 +1100

    Update 03_peak_booking_days.sql

--- sql/03_peak_booking_days.sql
+++ sql/03_peak_booking_days.sql
@@ -1,7 +1,7 @@
--- Peak booking days by number of bookings
+-- Peak booking months by number of bookings
 SELECT date,
        COUNT(*) AS bookings
 FROM bookings
-GROUP BY date
+GROUP BY month
 ORDER BY bookings DESC
 LIMIT 10;
